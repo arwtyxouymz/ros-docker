56760010922ddcc20688eaf2316ad455722a1a97
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2018-09-28 17:52:13 +0900

    changed build script

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 cd /catkin_ws
 
+if [ -e /catkin_ws/build/compile_commands.json ]; then
+    rm /catkin_ws/build/compile_commands.json
+fi
+
 catkin build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-jsons=`find /catkin_ws/src -name compile_commands.json`
+jsons=`find /catkin_ws/build -name compile_commands.json`
 jq -s '[.[][]]' $jsons > /catkin_ws/build/compile_commands.json
 
-cd /catkin_ws/src
+cd /catkin_ws/build
 
 if [ -x "$(command -v compdb)" ]
 then
